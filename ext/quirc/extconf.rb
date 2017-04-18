163d3aac82455f90656d4b28e8d33cf1100023b2
Jacob Middag <jacob@gaddim.nl>
2017-04-18 09:35:48 +0200

    Use makedirs to also create parent directories

--- ext/quirc/extconf.rb
+++ ext/quirc/extconf.rb
@@ -9,7 +9,7 @@ BUILD_DIR = File.join(TMP_DIR, "quirc-1.0")
 LIB_FILE = File.join(BUILD_DIR, "libquirc.a")
 
 def build_library
-  Dir.mkdir(BUILD_DIR) unless File.exist?(BUILD_DIR)
+  FileUtils.makedirs(BUILD_DIR)
   system({ "BUILD_DIR" => BUILD_DIR }, "make", "-C", SRC_DIR) or raise "Error building quirc"
 end
 
--- lib/quirc/version.rb
+++ lib/quirc/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Quirc
-  VERSION = "0.0.1".freeze
+  VERSION = "0.0.2".freeze
 end
